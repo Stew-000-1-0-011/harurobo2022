10daa340f4f1a2c23519a9bd84b3111c8e3a76a7
Stew-000-1-0-011 <92617876+Stew-000-1-0-011@users.noreply.github.com>
2022-03-06 14:51:35 +0900

    config修正。

--- include/harurobo2022/config.hpp
+++ include/harurobo2022/config.hpp
@@ -91,7 +91,7 @@ namespace Harurobo2022
                         inline constexpr std::uint16_t FR{0x610};
                         inline constexpr std::uint16_t FL{0x410};
                         inline constexpr std::uint16_t BL{0x4C4};
-                        inline constexpr std::uint16_t BR{0x520};
+                        inline constexpr std::uint16_t BR{0x400};
 
                         inline constexpr std::uint16_t all[4]{FR, FL, BL, BR};
                     }
@@ -99,14 +99,14 @@ namespace Harurobo2022
                     namespace LiftMotor
                     {
                         inline constexpr std::uint16_t FR{0x620};
-                        inline constexpr std::uint16_t FL{0x400};
+                        inline constexpr std::uint16_t FL{0x540};
                         inline constexpr std::uint16_t BL{0x510};
                         inline constexpr std::uint16_t BR{0x430};
 
                         inline constexpr std::uint16_t subX{0x448};
                         inline constexpr std::uint16_t subY{0x440};
 
-                        inline constexpr std::uint16_t collector{0x540};
+                        inline constexpr std::uint16_t collector{0x520};
                     }
 
                     inline constexpr std::uint16_t position_controll_ids[] = {LiftMotor::FR, LiftMotor::FL, LiftMotor::BL, LiftMotor::BR, LiftMotor::subX, LiftMotor::subY, LiftMotor::collector};
